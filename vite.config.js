28986d41b4c491f50b3ad1f0af259af520a7f758
myroslavkosyk81 <myroslav.kosyk@ukr.net>
2025-01-10 01:04:21 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+        // target: 'http://localhost:3000',
+        target: `${process.env.VITE_APP_API_URL}`,
+        changeOrigin: true,
+        secure: false,      
+        ws: true,
       },
     },
   },
   plugins: [react()],
 })
+
