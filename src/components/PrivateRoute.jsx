3c1b3026f958d3f1fce21c6c37ff0dba3d2fb973
myroslavkosyk81 <myroslav.kosyk@ukr.net>
2025-01-12 02:10:04 +0200

    private route

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,15 @@
 import { useSelector } from "react-redux";
 import { Outlet, Navigate } from "react-router-dom";
+import { useUser, useAuth } from '@clerk/clerk-react'
 
 export default function PrivateRoute() {
+const { getToken, isLoaded, isSignedIn, userId } = useAuth()
+const User = async () => {
+    const token = await getToken();
+    //   console.log(token)
+}
+ User();
+
     const {currentUser} = useSelector((state) => state.user);
-    return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
+    return User ? <Outlet /> : <Navigate to="/sign-in" />;
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -117,6 +117,43 @@ export default function Home() {
   }, []);
 
 
+  // useEffect(() => {
+  //   const fetchOfferListings = async () =>{
+  //     try {
+  //       const res = await fetch('/api/listing/get?offer=true&limit=4');
+  //       const data = await res.json();
+  //       // console.log(data)
+  //       setOfferListings(data);
+  //       fetchRentListings();
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
+
+  //   const fetchRentListings = async () => {
+  //     try {
+  //       const res = await fetch('/api/listing/get?type=rent&limit=4');
+  //       const data = await res.json();
+  //       setRentListings(data);
+  //       fetchSaleListings();
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
+
+  //   const fetchSaleListings = async () => {
+  //     try {
+  //       const res = await fetch('/api/listing/get?type=sale&limit=4');
+  //       const data = await res.json();
+  //       setSaleListings(data);
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
+  //   fetchOfferListings();
+  // }, []);
+
+
   return (
     <div>
       {/* top */}
